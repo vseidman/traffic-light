3f3f45bed696e5b31a2cf719f2a80dcd4a15161b
vseidman <victorseidman@gmail.com>
2022-07-21 23:10:04 +0000

    Proyecto Listo

---
+++ src/js/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Home from "./component/home.jsx"
+
+const Layout = () => {
+    return (
+        <Home/>
+    )
+}
+
+export default Layout
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	let [activeLight,setActiveLight] = useState("")
+	let [iterador,setIterador] = useState(0)
+	let handleLight= (color) => {
+		setActiveLight(color)
+	}
+	let colors = ["red","yellow","green"]
+	let cycleLights = () =>{
+		if(iterador == colors.length){
+			setIterador(0)
+			setActiveLight(colors[iterador])
+		}else{
+			setActiveLight(colors[iterador])
+			setIterador(iterador + 1)
+		}
+		
+	}
+
+	return ( 
+		<div className="container m-auto d-flex align-items-center flex-column">
+			<div className="d-flex justify-content-center poste"></div>
+			<div className="semaforo bg-dark p-2">
+			<div className={`red light ${activeLight === "red" ? "active" : ""}`} onClick={()=> handleLight("red")}></div>
+			<div className={`yellow light ${activeLight === "yellow" ? "active" : ""}`} onClick={()=> handleLight("yellow")}></div>	
+			<div className={`green light ${activeLight === "green" ? "active" : ""}`} onClick={()=> handleLight("green")}></div>
+			</div>
+			<button onClick={cycleLights} type="button" className="btn btn-primary my-3">Cambia de color</button>
 		</div>
 	);
 };
